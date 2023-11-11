9c6c6a2b96c970fbe2f7852ab76929903c0c59e3
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-12 00:20:33 +0900

    Feat: 정확한 메뉴입력 검증로직 추가

--- src/main/java/christmas/entity/validate/Validator.java
+++ src/main/java/christmas/entity/validate/Validator.java
@@ -1,10 +1,19 @@
 package christmas.entity.validate;
 
+import christmas.entity.menu.MenuItem;
+
+import java.util.List;
+
 public class Validator {
     private int MIN_DATE = 1;
     private int MAX_DATE = 31;
+    private int MIN_ORDER = 1;
     private String numberRegex = "^[0-9]*";
-    private String koreanRegex = "^[가-힣]";
+    private String orderRegex = "^([가-힣]+-[0-9]+,)+[가-힣]+-[0-9]+$";
+    List<String> menuList;
+    public Validator(){
+        menuList = MenuItem.getMenuNames();
+    }
     public void numberCheck(String rawInput){
         if(!rawInput.matches(numberRegex)){
             throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요.");
@@ -18,10 +27,21 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 올바른 날짜를 입력하세요.");
         }
     }
-    public void koreanCheck(String rawInput){
-        if(!rawInput.matches(koreanRegex)){
+    public void orderCheck(String rawInput){
+        if(!rawInput.matches(orderRegex)){
             throw new IllegalArgumentException("[ERROR] 올바른 메뉴를 입력하세요.");
         }
     }
+    public void orderRangeCheck(Integer input){
+        if(input<MIN_ORDER){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+    public void orderNameCheck(String rawInput){
+        if(!menuList.contains(rawInput)){
+            throw new IllegalArgumentException("[ERROR] 올바른 메뉴를 입력하세요.");
+        }
+    }
+
 
 }
