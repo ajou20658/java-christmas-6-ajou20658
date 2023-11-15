6cd63e50ed627d1c26e9bb701b496561f5adbc85
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-15 17:55:11 +0900

    Fix: 입력 포맷 맞추기 및 validator에 음료만 구매할 경우, 중복된 메뉴 입력시 예외뱉도록 수정

---
+++ src/main/java/christmas/model/validate/Validator.java
@@ -0,0 +1,72 @@
+package christmas.model.validate;
+
+import christmas.entity.menu.Menu;
+import christmas.entity.menu.MenuItem;
+import christmas.entity.menu.drink.Drink;
+
+import java.util.List;
+import java.util.Map;
+
+public class Validator {
+    private int MIN_DATE = 1;
+    private int MAX_DATE = 31;
+    private int MIN_ORDER = 1;
+    private int MAX_ORDER = 20;
+    private String numberRegex = "^[0-9]*";
+    private String orderRegex = "^[가-힣]+-[0-9]+(,[가-힣]+-[0-9]+)*$";
+    List<String> menuList;
+    public Validator(){
+        menuList = MenuItem.getMenuNames();
+    }
+    public void numberCheck(String rawInput){
+        if(!rawInput.matches(numberRegex)){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+    public void decemberDateRangeCheck(Integer input){
+        if(input>MAX_DATE){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        if(input<MIN_DATE){
+            throw new IllegalArgumentException("[ERROR] 올바른 날짜를 입력하세요.");
+        }
+    }
+    public void orderCheck(String rawInput){
+        if(!rawInput.matches(orderRegex)){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+    public void orderListCheck(Map<Menu,Integer> orderList){
+        orderOnlyDrink(orderList);
+        for(Menu i : orderList.keySet()){
+            orderRangeCheck(orderList.get(i));
+        }
+
+    }
+    public void orderRangeCheck(Integer input){
+        if(input<MIN_ORDER || input >MAX_ORDER){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+//    public void orderDuplicateCheck(Map<>)
+    public void orderOnlyDrink(Map<Menu,Integer> orderList){
+        int allCount = 0;
+        int drinkCount = 0;
+        for(Menu i: orderList.keySet()){
+            if(i.getClass()== Drink.class){
+                drinkCount += orderList.get(i);
+            }
+            allCount += orderList.get(i);
+        }
+        if(drinkCount==allCount){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+    public void orderNameCheck(String rawInput){
+        if(!menuList.contains(rawInput)){
+            throw new IllegalArgumentException("[ERROR] 올바른 메뉴를 입력하세요.");
+        }
+    }
+
+
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,16 +3,12 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.entity.menu.Menu;
 import christmas.entity.menu.MenuItem;
-import christmas.entity.validate.Validator;
+import christmas.model.validate.Validator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.List;
+import java.util.*;
 
 public class InputView {
     Validator validator = new Validator();
-
     public int readDate()throws IllegalArgumentException{
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         while (true) {
@@ -28,35 +24,35 @@ public class InputView {
             }
         }
     }
-    public List<Menu> readOrder(){
+    public Map<Menu,Integer> readOrder(){
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         while(true) {
             try{
                 String rawInput = Console.readLine();
                 validator.orderCheck(rawInput);
                 List<String> menuList = List.of(rawInput.split(","));
-                return orderToList(menuList);
+                Map<Menu,Integer> result = orderToList(menuList);
+                validator.orderListCheck(result);
+                return result;
             }catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }
         }
     }
     //메뉴의 개수대로 리스트를 반환해주는 함수
-    private List<Menu> orderToList(List<String> rawInput){
-        List<Menu> menuList = new ArrayList<>();
+    private Map<Menu,Integer> orderToList(List<String> rawInput){
+        Map<Menu,Integer> menuList = new HashMap<>();
         for(String input:rawInput){
             String[] split = input.split("-");
             validator.orderNameCheck(split[0]);
             Menu menu = menuMapping(split[0]);
-            menuList.addAll(menuCount(menu,Integer.parseInt(split[1])));
+            menuList.putAll(menuCount(menu,Integer.parseInt(split[1])));
         }
         return menuList;
     }
-    private List<Menu> menuCount(Menu name,Integer count){
-        List<Menu> menuList = new ArrayList<>();
-        for (int i=0;i<count;i++){
-            menuList.add(name);
-        }
+    private Map<Menu,Integer> menuCount(Menu name,Integer count){
+        Map<Menu,Integer> menuList = new HashMap<>();
+        menuList.put(name,count);
         return menuList;
     }
     private Menu menuMapping(String rawMenu){
