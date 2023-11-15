7fcb599700d180f72e77154192e27e50b9efbd45
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-15 18:36:50 +0900

    Feat: Map<Menu>에서 T extends Menu 에서 T별로 Map<T>를 얻는 역할의 전역 메서드 추가

--- src/main/java/christmas/filter/MenuFilter.java
+++ src/main/java/christmas/filter/MenuFilter.java
@@ -4,17 +4,16 @@ import christmas.entity.discount.Discount;
 import christmas.entity.menu.Menu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MenuFilter {
-    public static <T extends Menu> List<T> filterByType(
-            List<Menu> menuList, Class<? extends Discount<?>> type){
-    List<T> filteredList = new ArrayList<>();
-    for(Menu menu:menuList){
-        if(type.isInstance(menu)){
-            filteredList.add((T) menu);
-        }
-    }
-    return filteredList;
+
+    public static <T extends Menu, V> Map<Menu, Integer> filterByType(Map<? extends Menu, Integer> map, Class<T> type) {
+        return map.entrySet().stream()
+                .filter(entry -> type.isInstance(entry.getKey()))
+                .collect(Collectors.toMap(entry -> type.cast(entry.getKey()), Map.Entry::getValue));
     }
 }
