e558c3e7ed510496da0bb4f16a6d6f70d5c182f8
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-15 17:56:01 +0900

    Fix: validator 테스트 케이스 추가 음료만 받는 경우, 중복된 메뉴 입력한 경우

--- src/test/java/christmas/model/validate/ValidatorTest.java
+++ src/test/java/christmas/model/validate/ValidatorTest.java
@@ -1,12 +1,10 @@
-package christmas.entity.validate;
+package christmas.model.validate;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
     Validator validator = new Validator();
